91ec1425fa89667336308618e1122cbaedc05f75
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-12 17:13:12 +0200

    #3 Orchestrator Pattern

--- src/main/java/com/vinsguru/webfluxpatterns/sec03/client/InventoryClient.java
+++ src/main/java/com/vinsguru/webfluxpatterns/sec03/client/InventoryClient.java
@@ -1,5 +1,8 @@
 package com.vinsguru.webfluxpatterns.sec03.client;
 
+import com.vinsguru.webfluxpatterns.sec03.dto.Status;
+import com.vinsguru.webfluxpatterns.sec03.dto.request.InventoryRequest;
+import com.vinsguru.webfluxpatterns.sec03.dto.response.InventoryResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,6 +11,8 @@ import reactor.core.publisher.Mono;
 @Component
 public class InventoryClient {
 
+    private final static String RESTORE_ENDPOINT = "restore";
+    private final static String DEDUCT_ENDPOINT = "deduct";
     private final WebClient webClient;
 
     public InventoryClient(@Value("${sec03.inventory.service}") String baseUrl) {
@@ -22,5 +27,33 @@ public class InventoryClient {
                 .retrieve()
                 .bodyToMono(Integer.class)
                 .onErrorResume(ex -> Mono.empty());
+    }
+
+    public Mono<InventoryResponse> deduct(InventoryRequest request) {
+        return callUserService(request, DEDUCT_ENDPOINT);
+
+    }
+
+    public Mono<InventoryResponse> restore(InventoryRequest request) {
+        return callUserService(request, RESTORE_ENDPOINT);
+    }
+
+    private Mono<InventoryResponse> callUserService(InventoryRequest request, String endpoint) {
+        return this.webClient.post()
+                .uri(endpoint)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(InventoryResponse.class)
+                .onErrorReturn(buildErrorResponse(request));
+    }
+
+    private InventoryResponse buildErrorResponse(InventoryRequest request) {
+        return InventoryResponse.builder()
+                .productId(request.productId())
+                .quantity(request.quantity())
+                .status(Status.FAILED)
+                .build();
+
+
     }
 }
--- src/main/java/com/vinsguru/webfluxpatterns/sec03/client/ShippingClient.java
+++ src/main/java/com/vinsguru/webfluxpatterns/sec03/client/ShippingClient.java
@@ -1,17 +1,56 @@
 package com.vinsguru.webfluxpatterns.sec03.client;
 
+import com.vinsguru.webfluxpatterns.sec03.dto.Status;
+import com.vinsguru.webfluxpatterns.sec03.dto.User;
+import com.vinsguru.webfluxpatterns.sec03.dto.request.PaymentRequest;
+import com.vinsguru.webfluxpatterns.sec03.dto.request.ShippingRequest;
+import com.vinsguru.webfluxpatterns.sec03.dto.response.PaymentResponse;
+import com.vinsguru.webfluxpatterns.sec03.dto.response.ShippingResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ShippingClient {
 
+    private final static String SCHEDULE_ENDPOINT = "schedule";
+    private final static String CANCEL_ENDPOINT = "cancel";
     private final WebClient webClient;
 
     public ShippingClient(@Value("${sec03.shipping.service}") String baseUrl) {
         this.webClient = WebClient.builder()
                 .baseUrl(baseUrl)
                 .build();
+    }
+
+
+
+    public Mono<ShippingResponse> schedule(ShippingRequest request) {
+        return callUserService(request, SCHEDULE_ENDPOINT);
+
+    }
+
+    public Mono<ShippingResponse> cancel(ShippingRequest request) {
+        return callUserService(request, CANCEL_ENDPOINT);
+    }
+
+    private Mono<ShippingResponse> callUserService(ShippingRequest request, String endpoint) {
+        return this.webClient.post()
+                .uri(endpoint)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(ShippingResponse.class)
+                .onErrorReturn(buildErrorResponse(request));
+    }
+
+    private ShippingResponse buildErrorResponse(ShippingRequest request) {
+        return ShippingResponse.builder()
+                .orderId(request.orderId())
+                .quantity(request.quantity())
+                .status(Status.FAILED)
+                .build();
+
+
     }
 }

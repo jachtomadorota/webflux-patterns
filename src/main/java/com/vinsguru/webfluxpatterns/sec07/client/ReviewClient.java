f0c4d11bf6c6f26447181ee73ce86f8c953f903f
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-15 15:58:29 +0200

    #7 - Retry Pattern

--- src/main/java/com/vinsguru/webfluxpatterns/sec07/client/ReviewClient.java
+++ src/main/java/com/vinsguru/webfluxpatterns/sec07/client/ReviewClient.java
@@ -2,7 +2,6 @@ package com.vinsguru.webfluxpatterns.sec07.client;
 
 import com.vinsguru.webfluxpatterns.sec07.dto.Review;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
